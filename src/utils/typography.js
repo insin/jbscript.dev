642eff942b9003ead2a26843f7e78ff248cd578a
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-03-05 20:09:55 +1000

    Style tweaks - Make inline code lighter - Don't underline auto heading links

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,13 +1,14 @@
 import Typography from 'typography'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    'a.gatsby-resp-image-link': {
-      boxShadow: `none`,
-    },
-  }
-}
+Wordpress2016.overrideThemeStyles = () => ({
+  'a.gatsby-resp-image-link': {
+    boxShadow: `none`,
+  },
+  'a.anchor': {
+    boxShadow: 'none',
+  },
+})
 
 delete Wordpress2016.googleFonts
 
