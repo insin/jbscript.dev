ff4bc7d5eccafaa5703708c2585a08ec789693e1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-03-05 20:57:09 +1000

    Make loss felt on the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
+import { rhythm } from '../utils/typography'
 
 class NotFoundPage extends React.Component {
   render() {
@@ -11,10 +12,31 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <h1>Not Found</h1>
-        <p>
-          Oh, the sadness will never go / Will never go away / Baby it'&#39;s
-          here to stay...
-        </p>
+        <p>Did this page depart or did it never exist?</p>
+        <div
+          style={{
+            overflow: 'hidden',
+            position: 'relative',
+            paddingBottom: `${(315 / 560) * 100}%`,
+            marginBottom: rhythm(1),
+          }}
+        >
+          {/* eslint-disable-next-line */}
+          <iframe
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            src="https://www.youtube.com/embed/rJVhtkzIOHY?start=79"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullscreen
+          />
+        </div>
+        <p>Press play to feel its loss.</p>
       </Layout>
     )
   }
