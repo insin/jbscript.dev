d5421f76e881b1c21660a787f5673802e4a9dd97
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-03-07 04:58:24 +1000

    Avoid initial transition when starting in dark mode

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -59,7 +59,9 @@ class Layout extends React.Component {
     return (
       <div
         style={{
+          backgroundColor: 'var(--bg)',
           color: 'var(--textNormal)',
+          transition: 'color 0.2s ease-out, background 0.2s ease-out',
         }}
       >
         <div
