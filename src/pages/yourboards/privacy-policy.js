d31c3e4ddf9f10cbfef35cdb072e92a1217540bc
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-07-22 02:36:54 +1000

    Tweak privacy policy

--- src/pages/yourboards/privacy-policy.js
+++ src/pages/yourboards/privacy-policy.js
@@ -16,8 +16,13 @@ class PrivacyPolicy extends React.Component {
         <h1>Privacy Policy for Your Boards</h1>
         <img src={featureGraphic} alt="Your Boards title and logo"/>
         <h2>What information does the app collect?</h2>
-        <p>Your Boards does not collect any information from your device.</p>
-        <p>Your Boards requests use of the following on your device for the given purposes:</p>
+        <p>
+          Your Boards does not collect any personal or sensitive user data, or any other information
+          from your device.
+        </p>
+        <p>
+          Your Boards requests use of the following features of your device for the given purposes:
+        </p>
         <ul>
           <li>Camera &mdash; for taking photos and recording videos to be used in Your Boards.</li>
           <li>Microphone &mdash; for recording audio clips to be used in Your Boards.</li>
@@ -29,7 +34,7 @@ class PrivacyPolicy extends React.Component {
           elsewhere.
         </p>
         <p>
-          Details of your boards, items and the pictures, audio and video they use are stored and
+          Details of your boards and items, and the pictures, audio and video they use are stored and
           accessed locally by Your Boards and are never uploaded, posted, transmitted or otherwise
           made available elsewhere.
         </p>
