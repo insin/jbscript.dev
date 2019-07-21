c2367f5742f086023602a7905b748c3e5ba29392
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-07-22 01:40:00 +1000

    Add Privacy Policy page for Your Boards

---
+++ src/pages/yourboards/privacy-policy.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../../components/Layout'
+import Seo from '../../components/Seo'
+import featureGraphic from './feature-graphic.png'
+
+class PrivacyPolicy extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <Seo title="Privacy Policy for Your Boards"/>
+        <h1>Privacy Policy for Your Boards</h1>
+        <img src={featureGraphic} alt="Your Boards title and logo"/>
+        <h2>What information does the app collect?</h2>
+        <p>Your Boards does not collect any information from your device.</p>
+        <p>Your Boards requests use of the following on your device for the given purposes:</p>
+        <ul>
+          <li>Camera &mdash; for taking photos and recording videos to be used in Your Boards.</li>
+          <li>Microphone &mdash; for recording audio clips to be used in Your Boards.</li>
+          <li>External Storage &mdash; to access photos, videos and audio to be used in Your Boards.</li>
+        </ul>
+        <p>
+          Photos, videos and audio recorded from within Your Boards are stored and accessed locally
+          on your device and are never uploaded, posted, transmitted or otherwise made available
+          elsewhere.
+        </p>
+        <p>
+          Details of your boards, items and the pictures, audio and video they use are stored and
+          accessed locally by Your Boards and are never uploaded, posted, transmitted or otherwise
+          made available elsewhere.
+        </p>
+      </Layout>
+    )
+  }
+}
+
+export default PrivacyPolicy
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
